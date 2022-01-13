cdab68e97e67ead57077dca278ec4fd562735d44
Chris Prener <prenercg@CAS-AAS-9DQ6X9.ds.slu.edu>
2022-01-13 15:22:15 -0600

    start sp22 edits

--- build.R
+++ build.R
@@ -1,2 +1,19 @@
+## build
 bookdown::render_book("index.Rmd", "bookdown::gitbook")
 bookdown::render_book("index.Rmd", "bookdown::pdf_book")
+
+## confirm auto update data
+auto_update <- usethis::ui_yeah("Do you want to automatically update the remote GitHub repo?")
+
+## optionally pushed to GitHub
+if (auto_update == TRUE){
+  
+  message <- readline("What is your commit message?")
+  
+  system("git add -A")
+  system(paste0("git commit -a -m 'update site for ", as.character(Sys.Date()), " - ", message, "'"))
+  system("git push origin master")
+  
+}
+
+rm(auto_update, message)
